0df2be8dcf9a953403e4dede7d3ec2ff284f8abf
SAOULI-ROOT <hananesaoulih@gmail.com>
2021-09-30 23:47:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+  import React  from "react";
+  import'bootstrap/dist/css/bootstrap.min.css'
+  import './App.css';
+  import ReactRouter from "./Router";
+
+
+  export default function App() {
+    return<ReactRouter/>
+   }
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About () {
+    return (
+        <div>
+        <h1>     ABOUT PAGE      </h1>
+        <p> About definition, of; concerning; in regard to: instructions about the work; a book about the Civil War. See more.  </p>
+        </div>
+    )
+}
+
+  
